5005a9aab3cdc2c284aa5322269226a0126ea6a3
GiggleLiu <cacate0129@gmail.com>
2020-06-28 02:26:30 -0400

    clean up and add tests

--- test/vars.jl
+++ test/vars.jl
@@ -1,12 +1,7 @@
 using Test, NiLangCore
 
-@testset "anc" begin
-    NiLangCore.@anc x = 0.0
-    @test x === 0.0
-    NiLangCore.@deanc x = 0.0
-    @test x isa Nothing
+@testset "dataview" begin
     x = 1.0
-    @test_throws InvertibilityError (NiLangCore.@deanc x = 0.0)
     @assign value(x) 0.2
     @test x == 0.2
     @assign -x 0.1
@@ -14,6 +9,56 @@ using Test, NiLangCore
     x = 1+2.0im
     @assign x' 0.1+1im
     @test x == 0.1-1im
+    x = (3, 4)
+    @test tget(x, 1) == 3
+end
+
+@testset "anc, deanc" begin
+    @i function f(y)
+        x ← y
+        x → 1.0
+    end
+    f(1.0)
+    @test_throws InvertibilityError f(1.1)
+
+    @i function f(y)
+        x ← y
+        x → (1.0, 2.0)
+    end
+    f((1.0, 2.0))
+    @test_throws InvertibilityError f((1.1, 2.0))
+
+    @i function f(y)
+        x ← y
+        x → [1.0, 2.0]
+    end
+    f([1.0, 2.0])
+    @test_throws InvertibilityError f([1.1, 2.0])
+end
+
+@testset "inv and tuple output" begin
+    a, b = false, false
+    @instr ~(a ⊻= identity(true))
+    @test a == true
+    @instr ~((a, b) ⊻= identity((true, true)))
+    @test a == false
+    @test b == true
+    y = 1.0
+    x = 1.0
+    @instr ~(~(y += identity(1.0)))
+    @test y == 2.0
+    @instr ~(~((x, y) += identity((1.0, 1.0))))
+    @test y == 3.0
+    @test x == 2.0
+    @instr ~((x, y) += identity((1.0, 1.0)))
+    @test y == 2.0
+    @test x == 1.0
+    @instr ~(y += identity(1.0))
+    @test y == 1.0
+
+    z = [1.0, 2.0]
+    @instr ~(~(z .+= identity.([1.0, 2.0])))
+    @test z ≈ [2.0, 4.0]
 end
 
 @testset "chfield" begin
@@ -39,7 +84,8 @@ end
     x = Partial{:im}(3+2im)
     @test value(x) == 2
     @test chfield(x, value, 4) == Partial{:im}(3+4im)
-    (~Partial{:im})(x) == 3+2im
+    @test zero(x) == Partial{:im}(0.0+0.0im)
+    @test (~Partial{:im})(x) == 3+2im
 end
 
 @testset "pure wrapper" begin
