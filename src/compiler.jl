6c576066cb8fd7e78020e4eff0c4de8bc742b77b
GiggleLiu <cacate0129@gmail.com>
2021-05-03 20:11:22 -0400

    fix doctest

--- src/compiler.jl
+++ src/compiler.jl
@@ -276,9 +276,24 @@ export @code_julia
 
 Get the interpreted expression of `ex`.
 
-```jldoctest; setup=:(using NiLangCore)
+```julia
 julia> @code_julia x += exp(3.0)
-:(@assignback ((PlusEq)(exp))(x, 3.0) true)
+quote
+    var"##results#267" = ((PlusEq)(exp))(x, 3.0)
+    x = var"##results#267"[1]
+    try
+        (NiLangCore.deanc)(3.0, var"##results#267"[2])
+    catch e
+        @warn "deallocate fail: `3.0 → var\"##results#267\"[2]`"
+        throw(e)
+    end
+end
+
+julia> @code_julia @invcheckoff x += exp(3.0)
+quote
+    var"##results#257" = ((PlusEq)(exp))(x, 3.0)
+    x = var"##results#257"[1]
+end
 ```
 """
 macro code_julia(ex)
